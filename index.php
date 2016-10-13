8456d86fbb3a870e97eb9d96c18a692a1d197bb2
lokis <war.cratossai@gmail.com>
2016-10-13 18:52:54 +0530

    heroku needs this

--- index.php
+++ index.php
@@ -1,8 +1,12 @@
 <!DOCTYPE html/>
 <html lang="en" ng-app="ISTE">
+
 <head>
+
+	<base href="/">
 	<meta name="viewport" content="width=device-width,initial-scale=1">
 	<meta charset="utf-8">
+	
 	<title>ISTE GRIET</title>
 	<link rel="apple-touch-icon" sizes="57x57" href="icons/apple-icon-57x57.png">
 	<link rel="apple-touch-icon" sizes="60x60" href="icons/apple-icon-60x60.png">
@@ -17,70 +21,101 @@
 	<link rel="icon" type="image/png" sizes="32x32" href="icons/favicon-32x32.png">
 	<link rel="icon" type="image/png" sizes="96x96" href="icons/favicon-96x96.png">
 	<link rel="icon" type="image/png" sizes="16x16" href="icons/favicon-16x16.png">
+
 	<link rel="manifest" href="/manifest.json">
 	<meta name="msapplication-TileColor" content="#8B1734">
 	<meta name="msapplication-TileImage" content="icons/ms-icon-144x144.png">
 	<meta name="theme-color" content="#8B1734">
+	
 	<link rel="stylesheet" type="text/css" href="libsmin/semantic.min.css">
 	<link rel="stylesheet" type="text/css" href="app.min.css">
 </head>
+
+
 <body ng-controller="bodyCtrl as bodyvm">
 	<!--menu bar-->
+
 	<!--top menu start-->
 	<div class="ui fluid container main-menu">
 		<div class="ui secondary  menu">
-			<a href="#/">
+			<a href="/">
 				<li class="item home-tab" ng-class="{'active':bodyvm.activetab == 'home'}">
 					<img class="" src="/images/logos/istewhite.png" alt="Home tab">
 				</li>
 			</a>
-			<a href="#/events">
+
+			<a href="/events">
 				<li class="item" ng-class="{'active':bodyvm.activetab =='events'}">Events</li>
 			</a>
-			<a href="#/mela">
+			<a href="/mela">
 				<li class="item" ng-class="{'active':bodyvm.activetab =='mela'}">Mela</li>
 			</a>
-			<a href="#/team">
+
+			<a href="/team">
 				<li class="item" ng-class="{'active':bodyvm.activetab =='team'}">Team</li>
 			</a>
-			<a href="#/about">
+			<a href="/about">
 				<li class="item" ng-class="{'active':bodyvm.activetab =='about'}">About</li>
 			</a>
-			<a href="#/contact">
+			<a href="/contact">
 				<li class="item" ng-class="{'active':bodyvm.activetab =='contactUs'}">Contact</li>
 			</a>
+
+			<!--<div class="right menu">
+				<div class="item">
+					<div class="ui icon input">
+						<input type="text" placeholder="Search...">
+						<i class="search link icon"></i>
+					</div>
+				</div>
+
+			</div>-->
 		</div>
 	</div>
 	<!--top menu end-->
+
 	<div>
 		<ng-view></ng-view>
 	</div>
+
+
+
+
 	<div class="ui inverted vertical footer segment" style=" position: relative;
     top: 10%;">
+
+
+
+
 		<div class="ui center aligned container">
 			<div class="ui stackable inverted divided grid">
 				<div class="two wide column">
 					<h4 class="ui inverted header"><img class="ui image small" src="images/logos/istelogo.png" alt=""></h4>
 					<div class="ui inverted link list">
-						<!--<a href="#" class="item">Link One</a>-->
+						<!--<a href="" class="item">Link One</a>-->
+
 					</div>
 				</div>
+
 				<div class="eight wide column">
 					<h3 class="ui inverted header">Gokaraju Rangaraju Institute of Engineering and Technology</h3>
 					<h5 class="ui inverted header">(Approved by AICTE, Autonomous under JNTUH, Hyderabad)</h5>
 					<h5 class="ui inverted header">Bachupally, Kukatpally, Hyderabad-500090</h5>
 					<div class="ui inverted link list">
+
+
 					</div>
 				</div>
 				<div class="six wide column">
 					<h4 class="ui inverted header">Social Media</h4>
+
 					<!--social links-->
 					<div class="ui horizontal inverted large divided massive link list">
 						<a class="item" href="https://www.facebook.com/ISTEGRIETSB/"><i class="icon-facebook"></i></a>
 						<a href="https://twitter.com/istegrietsb" class="item"><i class="icon-twitter"></i></a>
 						<a href="mailto:istegrietsb@gmail.com" class="item"><i class="icon-mail"></i></a>
 						<a href="https://www.instagram.com/istegrietsb/?hl=en" class="item"><i class="icon-instagram"></i></a>
-						<a href="#/contact#contactnumber" class="item"><i class="icon-phone"></i> </a>
+						<a href="/contact#contactnumber" class="item"><i class="icon-phone"></i> </a>
 					</div>
 					<!--Social links-->
 				</div>
@@ -89,16 +124,24 @@
 			<a href="http://www.griet.ac.in/">
 			<img src="images/logos/grietlogo.png" alt="Griet logo" class="ui centered mini image">
 			</a>
+			
 			<div class="ui horizontal inverted small divided link list">
 				<a class="item" href="http://www.griet.ac.in/">GRIET</a>
-				<a class="item" href="#/contact">Contact Us</a>
-				<a class="item" href="#">Terms and Conditions</a>
-				<a class="item" href="#">Privacy Policy</a>
-				<a class="item" href="#">All Rights Reserved </a>
+				<a class="item" href="/contact">Contact Us</a>
+				<a class="item" href="">Terms and Conditions</a>
+				<a class="item" href="">Privacy Policy</a>
+				<a class="item" href="">All Rights Reserved </a>
 			</div>
 		</div>
+
+
+
 	</div>
+
+
 	<script src="libsmin/angular/angular.js"></script>
 	<script src="app.min.js"></script>
+
 </body>
+
 </html>
