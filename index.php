bc4d0851c124e8d3933276964d9d46b21ed6264b
lokis <war.cratossai@gmail.com>
2016-10-13 14:59:58 +0530

    small links changes

--- index.php
+++ index.php
@@ -1,11 +1,8 @@
 <!DOCTYPE html/>
 <html lang="en" ng-app="ISTE">
-
 <head>
 	<meta name="viewport" content="width=device-width,initial-scale=1">
 	<meta charset="utf-8">
-	
-
 	<link rel="apple-touch-icon" sizes="57x57" href="icons/apple-icon-57x57.png">
 	<link rel="apple-touch-icon" sizes="60x60" href="icons/apple-icon-60x60.png">
 	<link rel="apple-touch-icon" sizes="72x72" href="icons/apple-icon-72x72.png">
@@ -19,20 +16,15 @@
 	<link rel="icon" type="image/png" sizes="32x32" href="icons/favicon-32x32.png">
 	<link rel="icon" type="image/png" sizes="96x96" href="icons/favicon-96x96.png">
 	<link rel="icon" type="image/png" sizes="16x16" href="icons/favicon-16x16.png">
-
 	<link rel="manifest" href="/manifest.json">
 	<meta name="msapplication-TileColor" content="#8B1734">
 	<meta name="msapplication-TileImage" content="icons/ms-icon-144x144.png">
 	<meta name="theme-color" content="#8B1734">
-	
 	<link rel="stylesheet" type="text/css" href="libsmin/semantic.min.css">
 	<link rel="stylesheet" type="text/css" href="app.min.css">
 </head>
-
-
 <body ng-controller="bodyCtrl as bodyvm">
 	<!--menu bar-->
-
 	<!--top menu start-->
 	<div class="ui fluid container main-menu">
 		<div class="ui secondary  menu">
@@ -41,14 +33,12 @@
 					<img class="" src="/images/logos/istewhite.png" alt="Home tab">
 				</li>
 			</a>
-
 			<a href="#/events">
 				<li class="item" ng-class="{'active':bodyvm.activetab =='events'}">Events</li>
 			</a>
 			<a href="#/mela">
 				<li class="item" ng-class="{'active':bodyvm.activetab =='mela'}">Mela</li>
 			</a>
-
 			<a href="#/team">
 				<li class="item" ng-class="{'active':bodyvm.activetab =='team'}">Team</li>
 			</a>
@@ -61,42 +51,28 @@
 		</div>
 	</div>
 	<!--top menu end-->
-
 	<div>
 		<ng-view></ng-view>
 	</div>
-
-
-
-
 	<div class="ui inverted vertical footer segment" style=" position: relative;
     top: 10%;">
-
-
-
-
 		<div class="ui center aligned container">
 			<div class="ui stackable inverted divided grid">
 				<div class="two wide column">
 					<h4 class="ui inverted header"><img class="ui image small" src="images/logos/istelogo.png" alt=""></h4>
 					<div class="ui inverted link list">
 						<!--<a href="#" class="item">Link One</a>-->
-
 					</div>
 				</div>
-
 				<div class="eight wide column">
 					<h3 class="ui inverted header">Gokaraju Rangaraju Institute of Engineering and Technology</h3>
 					<h5 class="ui inverted header">(Approved by AICTE, Autonomous under JNTUH, Hyderabad)</h5>
 					<h5 class="ui inverted header">Bachupally, Kukatpally, Hyderabad-500090</h5>
 					<div class="ui inverted link list">
-
-
 					</div>
 				</div>
 				<div class="six wide column">
 					<h4 class="ui inverted header">Social Media</h4>
-
 					<!--social links-->
 					<div class="ui horizontal inverted large divided massive link list">
 						<a class="item" href="https://www.facebook.com/ISTEGRIETSB/"><i class="icon-facebook"></i></a>
@@ -109,7 +85,9 @@
 				</div>
 			</div>
 			<div class="ui inverted section divider"></div>
-			<img src="images/logos/grietlogo.png" class="ui centered mini image">
+			<a href="http://www.griet.ac.in/">
+			<img src="images/logos/grietlogo.png" alt="Griet logo" class="ui centered mini image">
+			</a>
 			<div class="ui horizontal inverted small divided link list">
 				<a class="item" href="http://www.griet.ac.in/">GRIET</a>
 				<a class="item" href="#/contact">Contact Us</a>
@@ -118,15 +96,8 @@
 				<a class="item" href="#">All Rights Reserved </a>
 			</div>
 		</div>
-
-
-
 	</div>
-
-
 	<script src="libsmin/angular/angular.js"></script>
 	<script src="app.min.js"></script>
-
 </body>
-
 </html>
